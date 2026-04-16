a4c8d9e753da29227dab487afd69b977c515c346
Sky Dev <freedev00.sky@gmail.com>
2026-04-16 15:08:28 -0400

    Updated 7

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import "./index.css";
 import App from "./App.jsx";
-import NotificationPage from "./NotificationPage.jsx";
 import { pumpGoTheme } from "./theme.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider theme={pumpGoTheme}>
       <CssBaseline />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/notifications" element={<NotificationPage />} />
-        </Routes>
-      </BrowserRouter>
+      <App />
     </ThemeProvider>
   </StrictMode>,
 );
